38fbce2c52b98efcc8ae74f7e75668e338b4d0da
Roxedus <roxedus@roxedus.net>
2019-03-06 18:36:55 +0100

    DevBranch stuff

--- alpine/radarr-collections/folders.sh
+++ alpine/radarr-collections/folders.sh
@@ -1,31 +1,10 @@
 #!/usr/bin/with-contenv bash
-if [[ ! -f /config/config.py ]]; then
-    echo "First run, cloning config.py into /config"
-    cp /app/config.default.py /config/config.py
-    ln -sf /config/config.py /app/config.py
+if [[ ! -f /config/words.conf ]]; then
+    echo "First run, cloning config into /config"
+    mv /app/config/* /config
+    ln -sf /config /app
 else
-    ln -sf /config/config.py /app/config.py
+    ln -sf /config /app
 fi
-if [[ ! -d /config/output ]]; then
-    echo "Output directory not existing, creating"
-    mkdir /config/output
-    ln -sf /config/output /app
-else
-    ln -sf /config/output /app
-fi
-if [[ ! -d /config/logs ]]; then
-    echo "Logs directory not existing, creating"
-    mkdir /config/logs
-    ln -sf /config/logs /app
-else
-    ln -sf /config/logs /app
-fi
-
-chown -R root:root \
-    /config/logs \
-    /config/output \
-    /config/config.py
-chmod -R 777 \
-    /config/logs \
-    /config/output \
-    /config/config.py
+chown -R root:root /config
+chmod -R 777 /config
